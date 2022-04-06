4b75168c42c338f306abb936c0f3ae692f9e2172
Alisha.Randall <a.mrandall@yahoo.com>
2022-04-06 13:40:49 -0600

    adding main.tf for azure rg deployments

---
+++ Main.tf
@@ -0,0 +1,15 @@
+terraform {
+    required_providers {
+      azurerm={
+          version="3.0.2"
+          source="hasicorp/azurerm"
+      }
+    }
+}
+
+provider "azurerm" {
+    features {
+
+    }
+  
+}
